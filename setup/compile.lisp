57e91b9b6f2648b994ff3dfc10375c63a13f8cdd
Alain Picard <Dr.Alain.Picard@gmail.com>
2013-03-13 13:08:52 +1100

    Forking the buildpack to load SBCL without invoking quicklisp on heroku. Just leave the whole build system to the heroku-setup function.

--- setup/compile.lisp
+++ setup/compile.lisp
@@ -1,57 +1,33 @@
 (in-package :cl-user)
 
-(defvar *build-dir* (pathname-directory (pathname (concatenate 'string (getenv "BUILD_DIR") "/"))))
-(defvar *cache-dir* (pathname-directory (pathname (concatenate 'string (getenv "CACHE_DIR") "/"))))
-(defvar *buildpack-dir* (pathname-directory (pathname (concatenate 'string (getenv "BUILDPACK_DIR") "/"))))
+#+sbcl (require :sb-posix)
 
-(format t "~%*build-dir* = ~a" *build-dir*)
-(format t "~%*cache-dir* = ~a" *cache-dir*)
-(format t "~%*buildpack-dir* = ~a" *buildpack-dir*)
+(defun heroku-getenv (target)
+  #+ccl (getenv target)
+  #+sbcl (sb-posix:getenv target))
 
-;;; Tell ASDF to store binaries in the cache dir
-(ccl:setenv "XDG_CACHE_HOME" (concatenate 'string (getenv "CACHE_DIR") "/.asdf/"))
+(defun heroku-setenv ()
+  #+ccl (ccl:setenv "XDG_CACHE_HOME" (concatenate 'string (heroku-getenv "CACHE_DIR") "/.asdf/"))
+  #+sbcl (sb-posix:putenv (format nil "XDG_CACHE_HOME=~A" (concatenate 'string (heroku-getenv "CACHE_DIR") "/.asdf/"))))
 
-(require :asdf)
+(defun env-var-to-path (var)
+  (make-pathname :defaults (format nil "~a/" (heroku-getenv var))))
 
-(let ((ql-setup (make-pathname :directory (append *cache-dir* '("quicklisp")) :defaults "setup.lisp")))
-  (if (probe-file ql-setup)
-      (load ql-setup)
-      (progn
-	(load (make-pathname :directory (append *buildpack-dir* '("lib")) :defaults "quicklisp.lisp"))
-	(funcall (symbol-function (find-symbol "INSTALL" (find-package "QUICKLISP-QUICKSTART")))
-		 :path (make-pathname :directory (pathname-directory ql-setup))))))
+(defvar *build-dir* (env-var-to-path "BUILD_DIR"))
+(defvar *cache-dir* (pathname-directory (pathname (concatenate 'string (heroku-getenv "CACHE_DIR") "/"))))
+(defvar *buildpack-dir* (pathname-directory (pathname (concatenate 'string (heroku-getenv "BUILDPACK_DIR") "/"))))
 
-;;; Load all .asd files in the repos subdirectory.  The compile script puts
-;;; several systems in there, because we are using versions that are 
-;;; different from those in Quicklisp. (update: Can't just load the files apparently,
-;;; have to add dirs to asdf:*central-registry*.  Blah.
-(let* ((asds (directory (make-pathname :directory  (append *cache-dir* '( "repos" :wild-inferiors))
-				       :name :wild
-				       :type "asd")))
-       (directories (remove-duplicates (mapcar #'pathname-directory asds) :test #'equal)))
-  (dolist (d directories)
-    (format t "~%~a added to asdf registry" d)
-    (push (make-pathname :directory d) asdf:*central-registry*)))
+;;; This loads the application
+(load (merge-pathnames "heroku-setup.lisp" *build-dir*))
 
-;;; App can redefine this to do runtime initializations
-(defun initialize-application (&key port)
-  )
+(defun h-save-app (app-file)
+  #+ccl (save-application app-file
+        :prepend-kernel t
+        :toplevel-function #'heroku-toplevel)
+  #+sbcl (sb-ext:save-lisp-and-die app-file
+        :toplevel #'heroku-toplevel
+        :executable t))
 
-(defvar *root* "/app")			;this is always the app root on Heroku now?
-
-;;; Default toplevel, app can redefine.
-(defun heroku-toplevel ()
-  (initialize-application :port (parse-integer (getenv "PORT")))
-  (loop (sleep 60))			;sleep forever
-  )
-
-;;; Load the application from sources
-(load (make-pathname :directory *build-dir* :defaults "heroku-compile.lisp"))
-
-;;; Save the application as an image
-(let ((app-file (format nil "~A/lispapp" (getenv "BUILD_DIR")))) ;must match path specified in bin/release
+(let ((app-file (format nil "~A/lispapp" (heroku-getenv "BUILD_DIR")))) ;must match path specified in bin/release
   (format t "Saving to ~A~%" app-file)
-  (save-application app-file
-		    :prepend-kernel t
-		    :toplevel-function #'heroku-toplevel
-		    ))
+  (h-save-app app-file))
